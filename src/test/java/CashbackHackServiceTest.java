6184c739811a2df17b87aa4b8675300d402c8b29
juulyya <yuliya.storozhenko.1994@mail.ru>
2023-05-19 22:42:42 +0400

    TestNg commit

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,36 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.netology.CashbackHackService;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void CashbackHackServiceTestOne (){
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 500;
+        int actual = service.remain(500);
+
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void CashbackHackServiceTestTwo (){
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 100;
+        int actual = service.remain(900);
+
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void CashbackHackServiceTestThree (){
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 800;
+        int actual = service.remain(200);
+
+        Assert.assertEquals(actual,expected);
+    }
+}
